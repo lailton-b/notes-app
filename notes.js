8eab34667a0a7ac0cf7e24a3f01db742e13e0134
Lailton <lailtoneletrohits@hotmail.com>
2021-11-13 11:20:04 -0300

    feat: add 'removeNote' to notes methods

--- notes.js
+++ notes.js
@@ -6,11 +6,11 @@ const addNote = (title, body) => {
   const isDuplicatedNote = notes.find((note) => note.title === title);
 
   if (isDuplicatedNote) {
-    const message = `A note with the title "${title}" already exists!`;
-    const separator = textSeparator(message.length);
-    console.log(chalk.green.bold(separator));
-    console.log(chalk.green.bold(message));
-    console.log(chalk.green.bold(separator));
+    const message = `Note already exists: ${chalk.white(`"${title}"`)}`;
+    const separator = textSeparator(message.length - 10);
+    console.log(chalk.red.bold(separator));
+    console.log(chalk.red.bold(message));
+    console.log(chalk.red.bold(separator));
     return;
   }
 
@@ -30,6 +30,36 @@ const addNote = (title, body) => {
   console.log(`\n`);
 };
 
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const noteIndex = notes.findIndex((note) => note.title === title);
+  const noteExists = noteIndex > -1;
+  if (!noteExists) {
+    const message = `Note not found: ${chalk.white(`"${title}"`)}`;
+    const separator = textSeparator(message.length - 10);
+    console.log(chalk.red.bold(separator));
+    console.log(chalk.red.bold(message));
+    console.log(chalk.red.bold(separator));
+    console.log(chalk.blue.bold('Title: '), chalk.white.bold(`${title}`));
+
+    return;
+  }
+
+  const noteBody = notes[noteIndex].body;
+  notes.splice(noteIndex, 1);
+  saveNotes(notes);
+
+  const message = `Note removed!`;
+  const separator = textSeparator(message.length);
+  console.log(chalk.green.bold(separator));
+  console.log(chalk.green.bold(message));
+  console.log(chalk.green.bold(separator));
+  console.log(chalk.blue.bold('Title: '), chalk.white.bold(`${title}`));
+  console.log(chalk.blue.bold('Body: '), chalk.white.bold(`${noteBody}`));
+  console.log(`\n`);
+};
+
 module.exports = {
   addNote,
+  removeNote,
 };
